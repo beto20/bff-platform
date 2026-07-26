01630595e7a6c6720a46debbe309948befd35c25
Alberto Velasquez <alberto.vs@outlook.com>
2026-07-26 16:57:09 -0500

    update cicd

--- src/main/java/com/bff/plataforma/client/PlatformClient.java
+++ src/main/java/com/bff/plataforma/client/PlatformClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 
-@FeignClient(name = "PlatformClient", url = "localhost:8082/api/v1/platforms")
+@FeignClient(name = "PlatformClient", url = "${url.platform}")
 public interface PlatformClient {
 
     @GetMapping
--- src/main/java/com/bff/plataforma/client/RepositoryClient.java
+++ src/main/java/com/bff/plataforma/client/RepositoryClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 
-@FeignClient(name = "RepositoryClient", url = "localhost:8083/api/v1/repositories")
+@FeignClient(name = "RepositoryClient", url = "${url.repo}")
 public interface RepositoryClient {
 
     @GetMapping
